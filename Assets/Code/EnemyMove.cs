56a273f1f2ecf38c8ba6454c25f881feaf4c696a
sunkang <kang.sun@joywinds.com>
2018-10-31 11:08:59 +0800

    代码提交

---
+++ Assets/Code/EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(2, 2);
+
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+    private Rigidbody2D rig;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        movement = new Vector2(
+            speed.x * direction.x,
+            speed.y * direction.y
+            );
+    }
+
+    private void FixedUpdate() {
+        if(rig == null)
+            rig = GetComponent<Rigidbody2D>();
+
+        rig.velocity = movement;
+    }
+}
