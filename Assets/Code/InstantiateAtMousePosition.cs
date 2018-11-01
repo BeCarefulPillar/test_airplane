6f2c68768ae3550fa7415be4feefb3cfdb6404c0
sunkang <465836845@qq.com>
2018-11-01 18:08:45 +0800

    点击出现

---
+++ Assets/Code/InstantiateAtMousePosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAtMousePosition : MonoBehaviour {
+    private Vector3 mousePosition, targetPosition;
+    private Transform fatherTrans;
+    private string prefabPaht = "Prefabs/";
+    private Transform myPrefab;
+
+    public string prefabName = "test";
+
+    // Use this for initialization
+    void Start() {
+        fatherTrans = GameObject.Find("Attack").transform;
+        myPrefab = ((GameObject)Resources.Load(prefabPaht + prefabName)).transform;
+        Instantiate(myPrefab, myPrefab.transform.position, myPrefab.transform.rotation, fatherTrans);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        mousePosition = Input.mousePosition;
+        
+        Vector3 pos = Camera.main.WorldToViewportPoint(fatherTrans.position);
+        targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x,mousePosition.y,pos.z));
+        Debug.Log(targetPosition.x);
+        Debug.Log(targetPosition.y);
+        myPrefab.transform.position = targetPosition;
+
+        if (myPrefab && Input.GetMouseButtonUp(0)) {
+            Instantiate(myPrefab, myPrefab.transform.position, myPrefab.transform.rotation, fatherTrans);
+        }
+
+    }
+}
